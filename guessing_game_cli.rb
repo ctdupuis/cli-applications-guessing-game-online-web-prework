6729c25ede0b1ca884280fa33d556f1cbcba5c6f
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-06 05:18:56 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,12 @@
 def run_guessing_game
   input = gets
+  rando = rand(7) + 1
   if input == 'exit'
     puts "Goodbye!"
+  elsif input == rando
+    puts "Correct!"
+  else input
+    puts "Sorry! The computer guessed #{rando}"
   end
   
 end
